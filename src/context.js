ca16d9700f2f192c1429c90ef849294de5a2fd19
Richard <82630326+richardsavala@users.noreply.github.com>
2021-08-08 14:58:54 -0600

    Add files via upload
    cart - clear cart
    in context.js const APpProvider added const clearCart

--- src/context.js
+++ src/context.js
@@ -10,16 +10,20 @@ const initialState = {
   loading: false,
   cart: cartItems,
   total: 0,
-  smount: 0,
+  amount: 0,
 };
 
 const AppProvider = ({ children }) => {
-  const [state, dispatch] = useReducer();
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const clearCart = () => {
+    dispatch({ type: "CLEAR_CART" });
+  };
 
   return (
     <AppContext.Provider
       value={{
-        cart,
+        ...state,
       }}
     >
       {children}
