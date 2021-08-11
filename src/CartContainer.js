92b52905e62a2e4977366563bf3b7241f645c6a6
Richard <82630326+richardsavala@users.noreply.github.com>
2021-08-10 22:31:08 -0600

    Add files via upload
    cart - remove item
    in context.js AppContext.Provider values, added remove

--- src/CartContainer.js
+++ src/CartContainer.js
@@ -32,7 +32,7 @@ const CartContainer = () => {
         <hr />
         <div className="cart-total">
           <h4>
-            total <span>{total}</span>
+            total <span>${total}</span>
           </h4>
         </div>
         <button className="btn clear-btn" onClick={clearCart}>
--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,14 @@
 const reducer = (state, action) => {
-    if (action.type === 'CLEAR_CART') {
-        return {...state, cart: [] }
+    if (action.type === "CLEAR_CART") {
+        return {...state, cart: [] };
     }
-    return state
+    if (action.type === "REMOVE") {
+        return {
+            ...state,
+            cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+        };
+    }
+    return state;
+};
 
-}
-
-export default reducer
+export default reducer;
