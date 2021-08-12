ba479361b1b1ed3c22be9d366258d0e676ad526a
Richard <82630326+richardsavala@users.noreply.github.com>
2021-08-12 13:44:14 -0600

    Add files via upload
    ill fix it after work

--- src/reducer.js
+++ src/reducer.js
@@ -30,14 +30,18 @@ const reducer = (state, action) => {
     }
     if (action.type === 'GET_TOTALS') {
 
-        const { total, amount } = state.cart.reduce((cartTotal, cartItem) => {
+        let { total, amount } = state.cart.reduce((cartTotal, cartItem) => {
             const { price, amount } = cartItem
+            const itemTotal = price * amount
+
+            cartTotal += itemTotal
             cartTotal.amount += amount
             return cartTotal
         }, {
             total: 0,
             amount: 0
         })
+        total = parseFloat(total.toFixed(2))
 
         return {...state, total, amount }
     }
