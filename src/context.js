2bdbf0ba867888c37595ffbba636684a6fe08b16
Richard <82630326+richardsavala@users.noreply.github.com>
2021-08-12 13:18:35 -0600

    Add files via upload
    cart - get totals
    in context.js const AppProvider added useEffect

--- src/context.js
+++ src/context.js
@@ -31,6 +31,11 @@ const AppProvider = ({ children }) => {
   const decrease = (id) => {
     dispatch({ type: "DECREASE", payload: id });
   };
+
+  useEffect(() => {
+    dispatch({ type: "GET_TOTALS" });
+  }, [state.cart]);
+
   return (
     <AppContext.Provider
       value={{
